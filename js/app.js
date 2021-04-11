8b41a0d82285fe3f3e6ddd72ecea9496eccd31cb
Miguel 'Chazz' Antunes <69439783+devchazz@users.noreply.github.com>
2021-04-11 10:25:51 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,56 @@
 function bhaskara(a, b, c) {
   const result = document.getElementById('result')
-  const operations = document.getElementById('operations')
-  const finalOperations = document.getElementById('final-operations')
+  const deltaOperations = document.getElementById('deltaOperations')
+  const XOperation = document.getElementById('x-operation')
+  const X1Operations = document.getElementById('x1-operations')
+  const X2Operations = document.getElementById('x2-operations')
+  const X1X2Results = document.getElementById('x1-x2-results')
+
 
   delta = b ** 2 - 4 * a * c
   
-  let deltaOperations = `
+  let resultDeltaOperations = `
   Solving: (${a}X²) + (${b}X) + (${c}) = 0 <br><br>
   DELTA: <br>
   delta = ${b}² - 4 * ${a} * ${c} <br>
   delta = ${b**2} - (${4*a*c}) <br>
   Delta = ${delta} <br>
   `
-  operations.innerHTML = deltaOperations
+  deltaOperations.innerHTML = resultDeltaOperations
 
   if (delta < 0) {
-    console.log('Delta é negativo')
-    console.log('-------------------------------------')
     result.innerHTML = 'Delta é negativo'
-    finalOperations.innerHTML = 'Delta é negativo'
+    XOperation.innerHTML = ''
+    X1Operations.innerHTML = ''
+    X2Operations.innerHTML = ''
+    X1X2Results.innerHTML = 'Delta é negativo'
   } else {
     let result1 = (-b + Math.sqrt(delta, 2)) / (2 * a)
     let result2 = (-b - Math.sqrt(delta, 2)) / (2 * a)
     result.innerHTML = 'X1 = ' + result1.toFixed(2) + ' | X2 = ' + result2.toFixed(2)
 
-    let finalResult = `
-    X = ${-b} +/- (&radic; ${delta}) / 2 <br><br>
-    X1: <br>
-    X1: ${-b} + ${Math.sqrt(delta, 2).toFixed(2)} / 2*${a} <br>
-    X1: ${(-b + Number((Math.sqrt(delta, 2)))).toFixed(2)} / ${2*a} <br>
-    X1: ${result1.toFixed(2)}<br><br>
-    X2: <br>
-    X2: ${-b} - ${Math.sqrt(delta, 2).toFixed(2)} / 2*${a} <br>
-    X2: ${-b - Math.sqrt(delta, 2).toFixed(2)} / ${2*a} <br>
-    X2: ${result2.toFixed(2)}<br><br>
-    X pode ser ${result1.toFixed(2)} ou ${result2.toFixed(2)} /// X = [ ${result1.toFixed(2)}, ${result2.toFixed(2)} ]
+    let Xresult = `<br>X = ${-b} +/- &radic;(${delta}) / 2 <br><br>`
+
+    let X1Result = `
+      X1: <br>
+      X1: ${-b} + ${Math.sqrt(delta, 2).toFixed(2)} / 2*${a} <br>
+      X1: ${(-b + Number((Math.sqrt(delta, 2)))).toFixed(2)} / ${2*a} <br>
+      X1: ${result1.toFixed(2)}<br><br>
+    `
+    let X2result = `
+      X2: <br>
+      X2: ${-b} - ${Math.sqrt(delta, 2).toFixed(2)} / 2*${a} <br>
+      X2: ${-b - Math.sqrt(delta, 2).toFixed(2)} / ${2*a} <br>
+      X2: ${result2.toFixed(2)}<br><br>
     `
 
-    finalOperations.innerHTML = finalResult
-    console.log(``)
+    let finalResult = `X can be ${result1.toFixed(2)} or ${result2.toFixed(2)} /// X = [ ${result1.toFixed(2)}, ${result2.toFixed(2)} ]`
+
+    XOperation.innerHTML = Xresult
+    X1Operations.innerHTML = X1Result
+    X2Operations.innerHTML = X2result
+
+    X1X2Results.innerHTML = finalResult
   }
 }
 
